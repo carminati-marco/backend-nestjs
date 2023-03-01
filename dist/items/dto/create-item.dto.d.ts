bc6cfe2bd5fe411013f1403614f0b65f493383c2
“carminati-marco” <marco.c@taboola.com>
2023-03-01 16:23:27 +0100

    .gitignore Fixed

--- dist/items/dto/create-item.dto.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class CreateItemDto {
-    title: string;
-    description: string;
-    picture: string;
-    link: string;
-    price: number;
-    discount: number;
-}
--- dist/items/entities/item.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Item {
-    id: number;
-    object: string;
-    picture: string;
-    link: string;
-    description: string;
-    price: number;
-    discount: number;
-}
